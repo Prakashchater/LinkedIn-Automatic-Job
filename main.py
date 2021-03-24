ba1bdae64c3438aaf95090e30a2e70e96dc9026c
Prakash Chater <53861867+Prakashchater@users.noreply.github.com>
2021-03-24 21:59:52 +0530

    update commit

--- main.py
+++ main.py
@@ -2,11 +2,11 @@ from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 import time
 from selenium.common.exceptions import NoSuchElementException
-PHONE = "7209914210"
-webdriver_path = "C:\Chrome driver\chromedriver.exe"
+PHONE = "YOUR PHONE NUMBER"
+webdriver_path = "YOUR PATH FILE"
 url = "https://www.linkedin.com/jobs/search/?f_LF=f_AL&geoId=102257491&keywords=python%20developer&location=London%2C%20England%2C%20United%20Kingdom&redirect=false&position=1&pageNum=0"
-email = "prakashchater@gmail.com"
-password1 = "pcchater@16"
+email = "YOUR LINKEDIN EMAIL ID"
+password1 = "YOUR PASSWORD"
 driver = webdriver.Chrome(executable_path=webdriver_path)
 driver.get(url)
 
